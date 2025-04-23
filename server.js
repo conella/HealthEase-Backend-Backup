bb7d52b96fea83f4fb70661736d6bcbdeb5be109
Conella <conellabelleth99@gmail.com>
2025-04-23 11:49:15 +0530

    chore: Fix failing tests

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT || 5432,
+  port: process.env.DB_PORT || 32879,
 });
 
 // Utility: JWT tokens
